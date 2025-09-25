bfa708af353da4f0994128087c523543862532e5
asarisaumya-coder <asarisaumya@gmail.com>
2025-09-25 15:19:03 +0530

    Add files via upload

---
+++ arr.2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+         int a[10];
+         int i;
+         printf("enter the values");
+         for(i=0;i<9;i++);
+         {
+              scanf("%d",&a[i]);
+
+          }
+
+
+             printf("%d",a[4]);
+             printf("%d",a[7]);
+             printf("%d",a[9]);
+
+
+
+
+
+
+
+                         }
+
+
