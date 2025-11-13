b1593a1c267becee1532b14ed1a846804fdeca50
asarisaumya-coder <asarisaumya@gmail.com>
2025-11-13 09:21:49 +0530

    Add files via upload

---
+++ uppercase.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+char str[100];
+puts("Enter a string: ");
+gets(str);
+
+strupr(str);
+
+ puts("the upper case string is ");
+ puts(str);
+ return 0;
+
+
+
+
+
+}
